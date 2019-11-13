707e402edf392d760ffc2e474b04f83ec96ef4db
aiybybz <aiybybz@163.com>
2019-11-13 17:32:04 +0800

    添加 系统广播 Demo

--- Broadcast/src/main/java/com/demon/broadcast/normal/NormalActivity.java
+++ Broadcast/src/main/java/com/demon/broadcast/normal/NormalActivity.java
@@ -27,11 +27,21 @@ public class NormalActivity extends AppCompatActivity {
 
     @OnClick(R.id.sendBroadcast)
     public void onViewClicked() {
-        Intent intent = new Intent();
-        // 对应 BroadcastReceiver 中 intentFilter 的 action
-        intent.setAction("NORMAL_ACTION");
-        // 发送广播
-        sendBroadcast(intent);
+//      // 隐式广播
+//      // Android8.0  targetSDK >= 26，禁止了所有的隐式广播的静态注册监听。
+//      Intent intent = new Intent();
+//      // 对应 BroadcastReceiver 中 intentFilter 的 action
+//      intent.setAction("NORMAL_ACTION");
+//      // 发送广播
+//      sendBroadcast(intent);
+
+        // 显式广播
+        Intent intentO = new Intent();
+        // 指定Action
+        intentO.setAction("NORMAL_ACTION");
+        // 指定包名
+        intentO.setPackage(getPackageName());
+        sendBroadcast(intentO);
     }
 
     public static void actionStart(Context context) {
